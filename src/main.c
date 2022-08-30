ceac6d312a4b2c9113abe2ff68906d52aaf612ed
atok <atoktoto@fastmail.fm>
2022-08-30 16:00:31 +0000

    Simplify CMakeLists.txt

--- src/main.c
+++ src/main.c
@@ -6,30 +6,19 @@
 #include "tusb.h"
 #if CFG_TUH_MIDI
 
-//--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
-//--------------------------------------------------------------------+
 void led_blinking_task(void);
-
 extern void midi_host_app_task(void);
 
-/*------------- MAIN -------------*/
-int main(void)
-{
+int main(void) {
+
   board_init();
-
   printf("TinyUSB Host MIDI Example\r\n");
-
   tusb_init();
 
-  while (1)
-  {
-    // tinyusb host task
+  while (1) {
     tuh_task();
     led_blinking_task();
-
     midi_host_app_task();
-
   }
 
   return 0;
@@ -37,24 +26,15 @@ int main(void)
 
 #endif
 
-//--------------------------------------------------------------------+
-// TinyUSB Callbacks
-//--------------------------------------------------------------------+
-
-//--------------------------------------------------------------------+
-// Blinking Task
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
+void led_blinking_task(void) {
   const uint32_t interval_ms = 1000;
   static uint32_t start_ms = 0;
 
   static bool led_state = false;
 
-  // Blink every interval ms
-  if ( board_millis() - start_ms < interval_ms) return; // not enough time
+  if ( board_millis() - start_ms < interval_ms) return;
   start_ms += interval_ms;
 
   board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+  led_state = 1 - led_state;
 }
