60fbfe5a4acfa84fa590c4c5eb2524f8881515c7
atok <atoktoto@fastmail.fm>
2022-10-03 12:56:09 +0200

    Working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,16 @@
 
-extern "C" {
 #include <sys/cdefs.h>
 #include <pico/platform.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#include <string.h>
-
 #include "FreeRTOS.h"
 #include "task.h"
 
 #include "bsp/board.h"
 #include "tusb.h"
-
 #include "class/midi/midi_host.h"
-#include "usb_descriptors.h"
-#include "queue.h"
-}
 
 #include "MIDI.h"
 #include "TUSB-MIDI.hpp"
@@ -34,41 +27,6 @@ using namespace midi;
 UsbMidiTransport usbMIDITransport(0);
 MidiInterface<UsbMidiTransport> MIDI((UsbMidiTransport&)usbMIDITransport);
 
-
-
-
-//--------------------------------------------------------------------+
-// TinyUSB Callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device with hid interface is mounted
-// Report descriptor is also available for use. tuh_hid_parse_report_descriptor()
-// can be used to parse common/simple enough descriptor.
-// Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE, it will be skipped
-// therefore report_desc = NULL, desc_len = 0
-void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx) {
-    TU_LOG1("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
-            dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
-
-    usbMIDITransport.midi_dev_addr = dev_addr;
-}
-
-// Invoked when device with hid interface is un-mounted
-void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance) {
-    TU_LOG1("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-
-    usbMIDITransport.midi_dev_addr = 0;
-}
-
-void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets) {
-    usbMIDITransport.tuh_midi_rx_cb(dev_addr, num_packets);
-}
-
-void tuh_midi_tx_cb(uint8_t dev_addr) {
-    (void) dev_addr;
-}
-
-
 void onNote(Channel channel, byte note, byte velocity) {
     printf("Note ON ch=%d, note=%d, vel=%d\n", channel, note, velocity);
 }
@@ -81,22 +39,15 @@ void onControlChange(Channel channel, byte b1, byte b2) {
     printf("ControlChange ch=%d, b1=%d, b2=%d\n", channel, b1, b2);
 }
 
-
-
-int main(void) {
+int main() {
 
     stdio_init_all();
     board_init();
+
     printf("TinyUSB Host MIDI Example\n");
 
-    xTaskCreate(led_blinking_task, "blink", 256, NULL, tskIDLE_PRIORITY, NULL);
-
-    TaskHandle_t tuh_task_handle;
-    xTaskCreate(midi_host_task, "tuh", USBD_STACK_SIZE, NULL, configMAX_PRIORITIES - 1, &(tuh_task_handle));
-    // UBaseType_t uxCoreAffinityMask;
-    // uxCoreAffinityMask = ( ( 1 << 0 ));`
-    // vTaskCoreAffinitySet( tuh_task_handle, uxCoreAffinityMask );
-
+    xTaskCreate(led_blinking_task, "blink", 256, nullptr, tskIDLE_PRIORITY, nullptr);
+    xTaskCreate(midi_host_task, "tuh", USBD_STACK_SIZE, nullptr, configMAX_PRIORITIES - 1, nullptr);
     xTaskCreate(midi_host_tx_task, "tuh-tx", USBD_STACK_SIZE, nullptr, tskIDLE_PRIORITY, nullptr);
 
     MIDI.begin(11);
@@ -135,12 +86,40 @@ _Noreturn void midi_host_task(void *pvParams) {
 _Noreturn void led_blinking_task(void *pvParams) {
     static bool led_state = false;
     while (true) {
-        printf("Blink\n");
         board_led_write(led_state);
-        led_state = 1 - led_state;
+        led_state = !led_state;
         vTaskDelay(200);
     }
 }
+
+//--------------------------------------------------------------------+
+// TinyUSB Callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device with hid interface is mounted
+void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx) {
+    printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
+            dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
+    usbMIDITransport.midi_dev_addr = dev_addr;
+}
+
+// Invoked when device with hid interface is un-mounted
+void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance) {
+    printf("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+    usbMIDITransport.midi_dev_addr = 0;
+}
+
+void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets) {
+    usbMIDITransport.tuh_midi_rx_cb(dev_addr, num_packets);
+}
+
+void tuh_midi_tx_cb(uint8_t dev_addr) {
+    (void) dev_addr;
+}
+
+//--------------------------------------------------------------------+
+// freeRTOS hooks
+//--------------------------------------------------------------------+
 
 extern "C" {
 void vApplicationMallocFailedHook(void) {
